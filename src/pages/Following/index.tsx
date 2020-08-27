cc63f253b32c5538e59e4eeca2119a5a8fcd0c60
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2020-08-26 22:47:31 -0300

    offline channels

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -6,6 +6,7 @@ import Heading from "../../components/Heading";
 import Title from "../../components/Title";
 import CategoryList from "../../components/CategoryList";
 import StreamList from "../../components/StreamList";
+import ChannelList from "../../components/ChannelList";
 
 import { Wrapper, Container, Main } from "./styles";
 
@@ -42,14 +43,14 @@ const Following: React.FC = () => {
 				render: () => <Title>Continue Watching</Title>,
 				isTitle: true,
 			},
-			{ key: "C3", render: () => <View /> },
+			{ key: "C3", render: () => <StreamList /> },
 
 			{
 				key: "OFFLINE_CHANNELS",
 				render: () => <Title>Offline Channels</Title>,
 				isTitle: true,
 			},
-			{ key: "C4", render: () => <View /> },
+			{ key: "C4", render: () => <ChannelList /> },
 		];
 
 		const indexes: number[] = [];
